8621bd9041ba7d6bc3f9f141ab89fa2a248fb9d5
Jonas Andersson <jonas@andersson.io>
2021-12-02 08:24:43 +0100

    2021-2

---
+++ 2021/2/2.py
@@ -0,0 +1,53 @@
+import pathlib
+from typing import Tuple
+
+
+def handle_command(cmd: str, depth, h_pos, aim=None) -> Tuple:
+    cmd = cmd.lower()
+    dir, amt = cmd.split()
+    amt = int(amt)
+    if dir == "forward":
+        if aim is not None:
+            depth += aim * amt
+        h_pos += amt
+    elif dir == "up":
+        if aim is not None:
+            aim -= amt
+        else:
+            depth -= amt
+    elif dir == "down":
+        if aim is not None:
+            aim += amt
+        else:
+            depth += amt
+    else:
+        raise RuntimeError("Woopsy")
+    return (depth, h_pos, aim)
+
+
+def part_one(data: list) -> None:
+    depth = 0
+    h_pos = 0
+    for command in data:
+        res = handle_command(command, depth, h_pos)
+        depth = res[0]
+        h_pos = res[1]
+    print(depth * h_pos)
+
+
+def part_two(data: list) -> None:
+    depth = 0
+    h_pos = 0
+    aim = 0
+    for command in data:
+        res = handle_command(command, depth, h_pos, aim)
+        depth = res[0]
+        h_pos = res[1]
+        aim = res[2]
+    print(depth * h_pos)
+
+
+with open(pathlib.Path(__file__).parent / "input.txt") as infile:
+    data = [line.strip() for line in infile.readlines()]
+part_one(data)
+part_two(data)
