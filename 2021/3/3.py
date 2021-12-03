f0498cd14cf96d3a2791e496e599c12c53c4f10b
Jonas Andersson <jonas@andersson.io>
2021-12-03 17:54:53 +0100

    Implement secret function, submarines probably do this?

--- 2021/3/3.py
+++ 2021/3/3.py
@@ -1,3 +1,4 @@
+"""The sea cannot claim us, Henrich. No ship is as seaworthy as ours."""
 import pathlib
 
 
@@ -5,18 +6,51 @@ def transpose(matrix: list) -> list:
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
 
 
-with open(pathlib.Path(__file__).parent / "input.txt") as infile:
-    matrix = [list(number.strip()) for number in infile.readlines()]
+def bin_mult(a: str, b: str) -> int:
+    return int(bin(int(a, 2) * int(b, 2)), 2)
 
-gamma = ""
-epsilon = ""
-for x in transpose(matrix):
-    num_ones = sum([int(y) for y in x])
-    num_zeroes = len(x) - num_ones
-    if num_ones > num_zeroes:
-        gamma += "1"
-        epsilon += "0"
-    else:
-        gamma += "0"
-        epsilon += "1"
-print(int(bin(int(gamma, 2) * int(epsilon, 2)), 2))
+
+def ___________(____):
+    "secret function"
+    _______ = "1"
+    ________ = "0"
+    ______________ = sum([int(_____) for _____ in ____])
+    if ______________ > len(____) - ______________:
+        return (_______, ________)
+    return (________, _______)
+
+
+if __name__ == "__main__":
+    with open(pathlib.Path(__file__).parent / "input.txt") as infile:
+        matrix = [list(number.strip()) for number in infile.readlines()]
+
+    matrix_t = transpose(matrix)
+    gamma = ""
+    epsilon = ""
+    for idx, x in enumerate(matrix_t):
+        num_ones = sum([int(y) for y in x])
+        num_zeroes = len(x) - num_ones
+        if num_ones > num_zeroes:
+            gamma += "1"
+            epsilon += "0"
+        else:
+            gamma += "0"
+            epsilon += "1"
+
+    print(bin_mult(gamma, epsilon))
+
+    oxy_rating = ""
+    co2_rating = ""
+
+    oxy_queue = []
+    co2_queue = []
+
+    oxy_bit, co2_bit = ___________(matrix_t[0])
+    for idx, bit in enumerate(matrix_t[0]):
+        if bit == oxy_bit:
+            oxy_queue.append(matrix[idx])
+    oxy_bit, co2_bit = ___________(oxy_queue)
+    for i in oxy_queue:
+        print(i)
+
+    # print(bin_mult(oxy_rating, co2_rating))
