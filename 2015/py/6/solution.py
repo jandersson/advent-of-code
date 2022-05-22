665b3fc84cac4111265433891442ae20c3a182e9
Jonas Andersson <jonas@andersson.io>
2022-05-22 17:30:36 -0500

    Finish part two, huzzah

--- 2015/py/6/solution.py
+++ 2015/py/6/solution.py
@@ -1,10 +1,11 @@
-"""Part 6"""
-from typing import List, Dict
+"""Day 6"""
+from typing import List, Dict, Tuple
 from pathlib import Path
 import re
 
 
 def load_input() -> List[str]:
+    """Load the input file as a list and trim the newline characters"""
     with open(
         Path(__file__).parents[2].absolute().resolve() / "input" / "6.txt",
         encoding="utf8",
@@ -12,20 +13,20 @@ def load_input() -> List[str]:
         return [line.strip() for line in infile.readlines()]
 
 
-def do_instruction(
+def do_instruction_part_one(
     instruction: str, grid: List[List[int]], start: tuple, end: tuple
 ) -> List[List[int]]:
     """
     Modify the grid according to the instruction and coordinates
 
-    >>> grid = [[0] * 10 for i in range(10)]
-    >>> do_instruction(instruction="toggle", grid=grid, start=(0,0), end=(2,2))
-    [[1, 1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    >>> grid = [[0] * 4 for i in range(4)]
+    >>> do_instruction_part_one(instruction="toggle", grid=grid, start=(0,0), end=(2,2))
+    [[1, 1, 1, 0], [1, 1, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]]
     """
     for row in range(start[0], end[0] + 1):
         for col in range(start[1], end[1] + 1):
             if instruction == "toggle":
-                grid[row][col] = grid[row][col] ^ 1
+                grid[row][col] ^= 1
             elif instruction == "turn on":
                 grid[row][col] = 1
             elif instruction == "turn off":
@@ -35,7 +36,34 @@ def do_instruction(
     return grid
 
 
-# %%
+def do_instruction_part_two(
+    grid: List[List[int]], instruction: str, start: Tuple, end: Tuple
+):
+    """
+    Modify the grid according to the instruction and coordinates
+    Instructions are modified from the first part:
+        * Turn on means increase brightness by 1
+        * Turn off means decrease brightness by 1 to a minimum of 0
+        * Toggle means increase brightness by 2
+
+    >>> grid = [[0] * 4 for i in range(4)]
+    >>> do_instruction_part_two(instruction="toggle", grid=grid, start=(0,0), end=(2,2))
+    [[2, 2, 2, 0], [2, 2, 2, 0], [2, 2, 2, 0], [0, 0, 0, 0]]
+    """
+    for row in range(start[0], end[0] + 1):
+        for col in range(start[1], end[1] + 1):
+            if instruction == "toggle":
+                grid[row][col] += 2
+            elif instruction == "turn on":
+                grid[row][col] += 1
+            elif instruction == "turn off":
+                if grid[row][col]:
+                    grid[row][col] -= 1
+            else:
+                raise RuntimeError("Instructions unclear. Set house on fire")
+    return grid
+
+
 def parse_instruction(inst: str) -> Dict:
     """
     Take a line from the input data and return an instruction dictionary
@@ -59,7 +87,6 @@ def parse_instruction(inst: str) -> Dict:
     return match_dict
 
 
-# %%
 if __name__ == "__main__":
     import doctest
 
@@ -72,16 +99,28 @@ if __name__ == "__main__":
     data = load_input()
     for line in data:
         instr = parse_instruction(line)
-        light_grid = do_instruction(
+        light_grid = do_instruction_part_one(
             instruction=instr["instruction"],
             grid=light_grid,
             start=instr["start"],
             end=instr["end"],
         )
     num_on = 0
-    for row in light_grid:
-        num_on += sum(row)
+    for grid_row in light_grid:
+        num_on += sum(grid_row)
     print(num_on)
 
-
-# %%
+    # Part two
+    light_grid = [[0] * 1000 for i in range(1000)]
+    for line in data:
+        instr = parse_instruction(line)
+        light_grid = do_instruction_part_two(
+            instruction=instr["instruction"],
+            grid=light_grid,
+            start=instr["start"],
+            end=instr["end"],
+        )
+    total_brightness = 0
+    for grid_row in light_grid:
+        total_brightness += sum(grid_row)
+    print(total_brightness)
